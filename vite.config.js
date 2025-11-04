a103a118406fc8df7dd03645600d21bec81f8a44
syxanash <si.marzulli@gmail.com>
2025-11-04 15:54:19 +0000

    add COOP and COEP headers

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,18 @@ import mkcert from 'vite-plugin-mkcert'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), mkcert()],
+  plugins: [
+    react(),
+    mkcert(),
+    {
+      name: 'isolation',
+      configureServer(server) {
+        server.middlewares.use((_req, res, next) => {
+          res.setHeader('Cross-Origin-Opener-Policy', 'same-origin');
+          res.setHeader('Cross-Origin-Embedder-Policy', 'require-corp');
+          next();
+        });
+      },
+    },
+  ],
 })
