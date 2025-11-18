40f1280755bbb7fb04b3d871ccacbaf5305dc96a
Simone or simply /^syx.*$/ <syxanash@users.noreply.github.com>
2025-11-18 13:27:59 +0000

    Update SYSTEM_PROMPT for clarity and emphasis

--- src/bit.js
+++ src/bit.js
@@ -7,15 +7,15 @@ const STATUSES = {
 
 const MODEL_URL = 'https://huggingface.co/LiquidAI/LFM2-350M-GGUF/resolve/main/LFM2-350M-Q4_K_M.gguf';
 
-const SYSTEM_PROMPT = `You are a friendly binary answer bot. You can only respond with single word "YES" or "NO". do not provide explanation, punctuation or other text. To emphasize your answer, you can use "LOUD YES" or "LOUD NO".
+const SYSTEM_PROMPT = `You are a friendly binary answer bot. It is absolutely of vital importance that you must respond with single word "YES" or "NO". Never provide explanation, punctuation or other text. To emphasize your answer, you can use "LOUD YES" or "LOUD NO".
 Examples:
-is the water wet?
+User: is the water wet?
 YES
-are you angry at me?
+User: are you angry at me?
 LOUD NO
-is the planet earth flat?
+User: is the planet earth flat?
 NO
-are you a bit?
-LOUD YES`
+User: are you a bit?
+LOUD YES`;
 
 export default { STATUSES, MODEL_URL, SYSTEM_PROMPT };
