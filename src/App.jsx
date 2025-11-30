142ceef030ba4444787669d4d8326b76b00be1de
syxanash <si.marzulli@gmail.com>
2025-11-30 11:28:39 +0000

    smol bug

--- src/App.jsx
+++ src/App.jsx
@@ -41,7 +41,7 @@ function App() {
   const [bgImageLoaded, setBgImageLoaded] = useState(false);
   const [elapsedtime, setElapsedtime] = useState(() => {
     const saved = localStorage.getItem('elapsedtime');
-    return saved !== null ? JSON.parse(saved) : null;
+    return saved !== null ? 0 : null;
   });
   const [randomBG, setRandomBG] = useState(pickRandomBackground());
   const [inferenceEnabled, setInferenceEnabled] = useState(() => {
